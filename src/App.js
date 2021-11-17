67fec697b34f8a5db33f251d0199499132a5a340
hanashiro <felipeaah@gmail.com>
2021-11-17 10:34:27 -0300

    aula-02-05

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
 import Index from './pages/index';
 import About from './pages/about';
 import Produtos, { Produto } from './pages/produtos';
-import { Routes, Route, Link, useRoutes } from 'react-router-dom';
+import {
+    Routes,
+    Route,
+    Link,
+    useLocation,
+    useNavigate,
+} from 'react-router-dom';
+import { useEffect } from 'react';
 
 function App() {
-    const element = useRoutes([
-        {
-            path: '/',
-            element: <Index />,
-        },
-        {
-            path: 'produtos/*',
-            element: <Produtos />,
-            children: [
-                {
-                    path: ':id',
-                    element: <Produto />,
-                },
-            ],
-        },
-    ]);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    console.log(location);
+
+    useEffect(() => {
+        if (location.pathname.includes('2')) {
+            navigate('/produtos/1');
+        }
+    }, [location, navigate]);
 
     return (
         <div>
-            {element}
+            <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="about/:id" element={<About />} />
+                <Route path="produtos/*" element={<Produtos />}>
+                    <Route path=":id" element={<Produto />} />
+                </Route>
+            </Routes>
 
-            {/* <Link to="/">Home</Link> */}
-            {/* <Link to="produtos">Produtos</Link> */}
+            <Link to="/">Home</Link>
+            <Link to="produtos">Produtos</Link>
             <br />
-            {/* <Link to="about/ABC">About 1</Link>
-            <Link to="about/DEF">About 2</Link> */}
+            <Link to="about/ABC">About 1</Link>
+            <Link to="about/DEF">About 2</Link>
         </div>
     );
 }
