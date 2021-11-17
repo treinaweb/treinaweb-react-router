2d8ff947db4f42b39844e96c6d313fa608eac73d
hanashiro <felipeaah@gmail.com>
2021-11-17 07:59:52 -0300

    aula-02-01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Index from './pages/index';
+import About from './pages/about';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="about" element={<About />} />
+            </Routes>
+
+            <Link to="/">Home</Link>
+            <br />
+            <Link to="about">About</Link>
+        </div>
+    );
 }
 
 export default App;
